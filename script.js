4e96e1d006bb60613226c5397fccd14e335fd875
ChewingTabasco <emerickjustice@gmail.com>
2022-07-14 17:07:16 -0400

    Update display on each button click

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", (e) => {
+    updateDisplay(e.target.textContent);
+  });
+});
+
+function updateDisplay(content) {
+  display.textContent += content;
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
